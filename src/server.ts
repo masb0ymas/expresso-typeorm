550c12c7ad939ea47d4404e90ca4e9948b6acb85
masb0ymas <fajriajjha@gmail.com>
2021-09-09 16:08:34 +0700

    feat: implementation typeorm

--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,19 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 require('@babel/register')({ extensions: ['.js', '.ts'] })
 
+import 'reflect-metadata'
+import { createConnection } from 'typeorm'
 import App from './app'
 
 const Server = new App()
+
+// connect to database
+createConnection()
+  .then(() => {
+    console.log('Connection has been established successfully.')
+  })
+  .catch((err) => {
+    console.error('Unable to connect to the database:', err)
+  })
 
 Server.run()
