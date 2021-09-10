37057821f97bc618c71a30b33f2cc94d89f360db
masb0ymas <fajriajjha@gmail.com>
2021-09-10 19:21:14 +0700

    feat: add express error yup

---
+++ src/middlewares/ExpressErrorYup.ts
@@ -0,0 +1,30 @@
+import { NextFunction, Request, Response } from 'express'
+import { ValidationError } from 'yup'
+
+async function ExpressErrorYup(
+  err: any,
+  req: Request,
+  res: Response,
+  next: NextFunction
+): Promise<Response<any, Record<string, any>> | undefined> {
+  if (err instanceof ValidationError) {
+    console.log('ERROR YUP VALIDATION!!!')
+
+    const error = {
+      code: 422,
+      message: err.errors.join('<br/>') || 'Yup Validation Error !',
+      errors:
+        err.inner.length > 0
+          ? err.inner.reduce((acc: any, curVal: any) => {
+              acc[`${curVal.path}`] = curVal.message || curVal.type
+              return acc
+            }, {})
+          : { [`${err.path}`]: err.message || err.type },
+    }
+    return res.status(422).json(error)
+  }
+
+  next(err)
+}
+
+export default ExpressErrorYup
