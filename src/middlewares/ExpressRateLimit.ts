c22a9915917fd3b27a087957dbc9c70d13f7722e
masb0ymas <fajriajjha@gmail.com>
2021-09-10 16:49:13 +0700

    feat: add middleware rate limit

---
+++ src/middlewares/ExpressRateLimit.ts
@@ -0,0 +1,32 @@
+/* eslint-disable @typescript-eslint/no-invalid-void-type */
+import redisClient from '@config/redisClient'
+import dotenv from 'dotenv'
+import { NextFunction, Request, Response } from 'express'
+import { RateLimiterRedis } from 'rate-limiter-flexible'
+
+dotenv.config()
+
+const RATE_LIMIT = Number(process.env.RATE_LIMIT) || 50
+
+// Rate Limit Request
+const rateLimiter = new RateLimiterRedis({
+  storeClient: redisClient,
+  keyPrefix: 'middleware',
+  points: RATE_LIMIT, // 10 requests
+  duration: 1, // per 1 second by IP
+})
+
+async function ExpressRateLimit(
+  req: Request,
+  res: Response,
+  next: NextFunction
+): Promise<void | Response<any, Record<string, any>>> {
+  try {
+    await rateLimiter.consume(req.ip)
+    return next()
+  } catch (err) {
+    return res.status(429).json({ code: 429, message: 'Too Many Requests' })
+  }
+}
+
+export default ExpressRateLimit
