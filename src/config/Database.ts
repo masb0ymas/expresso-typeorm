89df3da471e5b78ddcd70b728a6b1cbbec09b81b
masb0ymas <fajriajjha@gmail.com>
2021-09-10 09:58:49 +0700

    fix: config database

---
+++ src/config/Database.ts
@@ -0,0 +1,28 @@
+import { validateBoolean } from '@expresso/helpers/Common'
+import path from 'path'
+import { ConnectionOptions } from 'typeorm'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const dbConfig: ConnectionOptions = {
+  type: 'mysql',
+  host: process.env.TYPEORM_HOST ?? '127.0.0.1',
+  port: Number(process.env.TYPEORM_PORT) ?? 3306,
+  username: process.env.TYPEORM_USERNAME ?? 'test',
+  password: process.env.TYPEORM_PASSWORD ?? 'test',
+  database: process.env.TYPEORM_DATABASE ?? 'example',
+  timezone: process.env.TYPEORM_TIMEZONE ?? '+07:00',
+  synchronize: validateBoolean(process.env.TYPEORM_SYNCHRONIZE) ?? true,
+  logging: validateBoolean(process.env.TYPEORM_LOGGING) ?? false,
+  entities: [path.resolve(`${__dirname}/../entity/**/*.ts`)],
+  migrations: [path.resolve(`${__dirname}/../migration/**/*.ts`)],
+  subscribers: [path.resolve(`${__dirname}/../subscriber/**/*.ts`)],
+  cli: {
+    entitiesDir: path.resolve(`${__dirname}/../entity`),
+    migrationsDir: path.resolve(`${__dirname}/../migration`),
+    subscribersDir: path.resolve(`${__dirname}/../subscriber`),
+  },
+}
+
+export default dbConfig
