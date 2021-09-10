846c844390dc1e54f369b962c790710d4864e500
masb0ymas <fajriajjha@gmail.com>
2021-09-10 20:19:38 +0700

    fix: field entity user

--- src/entity/User.ts
+++ src/entity/User.ts
@@ -4,9 +4,25 @@ import {
   Column,
   CreateDateColumn,
   UpdateDateColumn,
+  Unique,
 } from 'typeorm'
 
+interface UserAttributes {
+  id?: string
+  firstName: string
+  lastName: string
+  email: string
+  password: string
+  phone: string | null
+  RoleId: string
+  createdAt: Date
+  updatedAt: Date
+}
+
+export type UserPost = Omit<UserAttributes, 'id' | 'createdAt' | 'updatedAt'>
+
 @Entity()
+@Unique(['email'])
 export class User {
   @PrimaryGeneratedColumn('uuid')
   id: string
@@ -20,8 +36,14 @@ export class User {
   @Column()
   email: string
 
+  @Column()
+  password: string
+
   @Column('char', { length: 20 })
   phone: string
+
+  @Column('uuid')
+  RoleId: string
 
   @CreateDateColumn()
   createdAt: Date
