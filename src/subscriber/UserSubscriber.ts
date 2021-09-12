8bf324d5f0aad9b026f6f27cbcb1e8e16a7da2f7
masb0ymas <fajriajjha@gmail.com>
2021-09-12 17:57:49 +0700

    feat: add subscriber user

---
+++ src/subscriber/UserSubscriber.ts
@@ -0,0 +1,34 @@
+import { User } from '@entity/User'
+import bcrypt from 'bcrypt'
+import {
+  EventSubscriber,
+  EntitySubscriberInterface,
+  InsertEvent,
+  UpdateEvent,
+} from 'typeorm'
+
+const saltRound = 10
+
+@EventSubscriber()
+export class UserSubscriber implements EntitySubscriberInterface<any> {
+  listenTo(): typeof User {
+    return User
+  }
+
+  async hashPassword(entity: User): Promise<void> {
+    entity.password = await bcrypt.hash(entity.password, saltRound)
+  }
+
+  beforeInsert(event: InsertEvent<User>): Promise<void> | undefined {
+    if (event.entity.password) {
+      return this.hashPassword(event.entity)
+    }
+  }
+
+  async beforeUpdate(event: UpdateEvent<User>): Promise<void> {
+    if (event.entity?.password !== event.databaseEntity?.password) {
+      // @ts-expect-error
+      await this.hashPassword(entity)
+    }
+  }
+}
