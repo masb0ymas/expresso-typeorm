85338d20e9cf3a49add751e739c35cd143154493
masb0ymas <fajriajjha@gmail.com>
2021-09-21 20:49:31 +0700

    fix: typing express

--- src/types/express/index.d.ts
+++ src/types/express/index.d.ts
@@ -1,7 +1,13 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import withState from '@expresso/helpers/withState'
+import multer from 'multer'
 
 declare global {
   namespace Express {
+    // interface Multer {
+    //   File: any
+    // }
     interface Request extends withState {
       state: object
     }
