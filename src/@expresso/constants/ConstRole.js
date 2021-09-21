3ab23b7bc10c450a7192665c18a965906912cbba
masb0ymas <fajriajjha@gmail.com>
2021-09-21 20:46:53 +0700

    feat: add constant

---
+++ src/@expresso/constants/ConstRole.js
@@ -0,0 +1,9 @@
+const ID_ADMIN = 'ec1c7f9b-f78d-4feb-a9a8-60f9f47cabc1'
+const ID_USER = 'd6acc507-a192-4ce9-943e-af41763d6d46'
+
+const ConstRole = {
+  ID_ADMIN,
+  ID_USER,
+}
+
+module.exports = ConstRole
