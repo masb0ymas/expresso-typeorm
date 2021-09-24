18f1d39f8d96d20b8d0e51323936351b98b1cf70
masb0ymas <fajriajjha@gmail.com>
2021-09-24 16:50:43 +0700

    feat: add middleware auth

---
+++ src/middlewares/Authorization.ts
@@ -0,0 +1,24 @@
+import { currentToken, verifyAccessToken } from '@expresso/helpers/Token'
+import { NextFunction, Request, Response } from 'express'
+import _ from 'lodash'
+
+async function Authorization(
+  req: Request,
+  res: Response,
+  next: NextFunction
+): Promise<Response<any, Record<string, any>> | undefined> {
+  const getToken = currentToken(req)
+  const token = verifyAccessToken(getToken)
+
+  if (_.isEmpty(token?.data)) {
+    return res.status(401).json({
+      code: 401,
+      message: token?.message,
+    })
+  }
+
+  req.setState({ userLogin: token?.data })
+  next()
+}
+
+export default Authorization
