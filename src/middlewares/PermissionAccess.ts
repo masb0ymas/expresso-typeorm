5831ebf110d1f04e3e50ba9861293b9d4f4d5d01
masb0ymas <fajriajjha@gmail.com>
2021-09-24 16:50:57 +0700

    feat: add middleware permission access

---
+++ src/middlewares/PermissionAccess.ts
@@ -0,0 +1,29 @@
+import UserService from '@controllers/User/service'
+import { UserLoginAttributes } from '@entity/User'
+import HttpResponse from '@expresso/modules/Response/HttpResponse'
+import chalk from 'chalk'
+import { NextFunction, Request, Response } from 'express'
+
+function PermissionAccess(roles: string[]) {
+  return async (req: Request, res: Response, next: NextFunction) => {
+    const userLogin = req.getState('userLogin') as UserLoginAttributes
+    const getUser = await UserService.findById(userLogin.uid)
+
+    if (!roles.includes(getUser.RoleId)) {
+      const errType = `Permission Access Error:`
+      const message = 'You are not allowed'
+      console.log(chalk.red(errType), chalk.green(message))
+
+      const httpResponse = HttpResponse.get({
+        code: 403,
+        message: `${errType} ${message}`,
+      })
+
+      return res.status(403).json(httpResponse)
+    }
+
+    next()
+  }
+}
+
+export default PermissionAccess
