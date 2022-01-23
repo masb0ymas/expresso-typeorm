d42afd3c8acf42c847d3a39cca3adb5df3c6a182
N. Fajri <fajriajjha@gmail.com>
2022-01-23 19:49:04 +0700

    fix: role seeder

--- src/database/migrations/1642501604297-RoleSeeder.ts
+++ src/database/migrations/1642501604297-RoleSeeder.ts
@@ -2,10 +2,16 @@ import { Role } from '@database/entities/Role'
 import ConstRole from '@expresso/constants/ConstRole'
 import { getRepository, MigrationInterface, QueryRunner } from 'typeorm'
 
-export class seeds1642339024317 implements MigrationInterface {
+export class RoleSeeder1642501604297 implements MigrationInterface {
   public async up(_: QueryRunner): Promise<void> {
     // save
     await getRepository(Role).save([
+      {
+        id: ConstRole.ID_SUPER_ADMIN,
+        name: 'Super Admin',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
       {
         id: ConstRole.ID_ADMIN,
         name: 'Admin',
