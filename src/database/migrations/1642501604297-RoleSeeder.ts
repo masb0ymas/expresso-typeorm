603c296c180eb2a3eda0c2e84467df7b72928cc9
N. Fajri <fajriajjha@gmail.com>
2022-03-09 14:37:03 +0700

    fix: seeder

--- src/database/migrations/1642501604297-RoleSeeder.ts
+++ src/database/migrations/1642501604297-RoleSeeder.ts
@@ -1,30 +1,41 @@
 import { Role } from '@database/entities/Role'
 import ConstRole from '@expresso/constants/ConstRole'
+import _ from 'lodash'
 import { getRepository, MigrationInterface, QueryRunner } from 'typeorm'
+
+const data = [
+  {
+    id: ConstRole.ID_SUPER_ADMIN,
+    name: 'Super Admin',
+  },
+  {
+    id: ConstRole.ID_ADMIN,
+    name: 'Admin',
+  },
+  {
+    id: ConstRole.ID_USER,
+    name: 'User',
+  },
+]
+
+const formData: any[] = []
+
+if (!_.isEmpty(data)) {
+  for (let i = 0; i < data.length; i += 1) {
+    const item = data[i]
+
+    formData.push({
+      ...item,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    })
+  }
+}
 
 export class RoleSeeder1642501604297 implements MigrationInterface {
   public async up(_: QueryRunner): Promise<void> {
     // save
-    await getRepository(Role).save([
-      {
-        id: ConstRole.ID_SUPER_ADMIN,
-        name: 'Super Admin',
-        createdAt: new Date(),
-        updatedAt: new Date(),
-      },
-      {
-        id: ConstRole.ID_ADMIN,
-        name: 'Admin',
-        createdAt: new Date(),
-        updatedAt: new Date(),
-      },
-      {
-        id: ConstRole.ID_USER,
-        name: 'User',
-        createdAt: new Date(),
-        updatedAt: new Date(),
-      },
-    ])
+    await getRepository(Role).save(formData)
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
--- src/database/migrations/1642508834295-UserSeeder.ts
+++ src/database/migrations/1642508834295-UserSeeder.ts
@@ -1,45 +1,50 @@
 import { User } from '@database/entities/User'
 import ConstRole from '@expresso/constants/ConstRole'
+import _ from 'lodash'
 import { getRepository, MigrationInterface, QueryRunner } from 'typeorm'
 import { v4 as uuidv4 } from 'uuid'
 
 const defaultPass = 'Padang123'
 
+const data = [
+  {
+    fullName: 'Super Admin',
+    email: 'super.admin@mail.com',
+    RoleId: ConstRole.ID_SUPER_ADMIN,
+  },
+  {
+    fullName: 'Admin',
+    email: 'admin@mail.com',
+    RoleId: ConstRole.ID_ADMIN,
+  },
+  {
+    fullName: 'User',
+    email: 'user@mail.com',
+    RoleId: ConstRole.ID_USER,
+  },
+]
+
+const formData: any[] = []
+
+if (!_.isEmpty(data)) {
+  for (let i = 0; i < data.length; i += 1) {
+    const item = data[i]
+
+    formData.push({
+      ...item,
+      id: uuidv4(),
+      isActive: true,
+      password: defaultPass,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    })
+  }
+}
+
 export class UserSeeder1642508834295 implements MigrationInterface {
   public async up(_: QueryRunner): Promise<void> {
     // save
-    await getRepository(User).save([
-      {
-        id: uuidv4(),
-        fullName: 'Super Admin',
-        email: 'super.admin@mail.com',
-        password: defaultPass,
-        isActive: true,
-        RoleId: ConstRole.ID_SUPER_ADMIN,
-        createdAt: new Date(),
-        updatedAt: new Date(),
-      },
-      {
-        id: uuidv4(),
-        fullName: 'Admin',
-        email: 'admin@mail.com',
-        password: defaultPass,
-        isActive: true,
-        RoleId: ConstRole.ID_ADMIN,
-        createdAt: new Date(),
-        updatedAt: new Date(),
-      },
-      {
-        id: uuidv4(),
-        fullName: 'User',
-        email: 'user@mail.com',
-        password: defaultPass,
-        isActive: true,
-        RoleId: ConstRole.ID_USER,
-        createdAt: new Date(),
-        updatedAt: new Date(),
-      },
-    ])
+    await getRepository(User).save(formData)
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
