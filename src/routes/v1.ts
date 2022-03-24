d2edd90768d75e2fe5d64998de707c431108cc4b
N. Fajri <fajriajjha@gmail.com>
2022-03-24 13:11:47 +0700

    fix: mapping route

--- src/routes/v1.ts
+++ src/routes/v1.ts
@@ -1,16 +1,27 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 import Express from 'express'
+import fs from 'fs'
+import path from 'path'
 
 const route = Express.Router()
 
+const baseRoutes = path.resolve(`${__dirname}/../controllers`)
+
+/**
+ * Get Routes
+ * @param basePath
+ */
+const getRoutes = (basePath: string | Buffer): void => {
+  return fs.readdirSync(basePath).forEach((file) => {
+    // read controller
+    const getController = `${baseRoutes}/${file}/controller`
+
+    // require controller
+    require(getController)
+  })
+}
+
 export default route
 
-// Auth
-require('@controllers/Auth/controller')
-
-// Upload
-require('@controllers/Upload/controller')
-
-// Account
-require('@controllers/Role/controller')
-require('@controllers/Session/controller')
-require('@controllers/User/controller')
+// Mapping Route
+getRoutes(baseRoutes)
