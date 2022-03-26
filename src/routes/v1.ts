4c33f35d5f623cfd593cf30d5547c94d7ec90cb0
N. Fajri <fajriajjha@gmail.com>
2022-03-26 15:00:14 +0700

    feat: add helper routing

--- src/routes/v1.ts
+++ src/routes/v1.ts
@@ -1,25 +1,10 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
+import { getRoutes } from '@expresso/helpers/Routing'
 import Express from 'express'
-import fs from 'fs'
 import path from 'path'
 
 const route = Express.Router()
 
 const baseRoutes = path.resolve(`${__dirname}/../controllers`)
-
-/**
- * Get Routes
- * @param basePath
- */
-const getRoutes = (basePath: string | Buffer): void => {
-  return fs.readdirSync(basePath).forEach((file) => {
-    // read controller
-    const getController = `${baseRoutes}/${file}/controller`
-
-    // require controller
-    require(getController)
-  })
-}
 
 export default route
 
