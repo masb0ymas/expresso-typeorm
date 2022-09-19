6fadda22edcb4c1492e261d6b8240dfe14e56b5e
N. Fajri <fajriajjha@gmail.com>
2022-09-19 11:27:41 +0700

    fix: script drop db

--- src/scripts/dropDB.ts
+++ src/scripts/dropDB.ts
@@ -3,5 +3,9 @@ import { AppDataSource } from '../database/data-source'
 
 void (async () => {
   // Create the database with specification of the DataSource options
-  await dropDatabase({ options: AppDataSource.options, ifExist: true })
+  await dropDatabase({
+    options: AppDataSource.options,
+    initialDatabase: 'postgres',
+    ifExist: true,
+  })
 })()
