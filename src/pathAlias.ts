45296e054ab56fa45d0ed6c81f1a54548c649947
masb0ymas <n.fajri@outlook.com>
2023-02-16 22:55:54 +0700

    fix: name module alias database

--- src/pathAlias.ts
+++ src/pathAlias.ts
@@ -4,6 +4,7 @@ ModuleAlias.addAliases({
   '@core': `${__dirname}/@core`,
   '@config': `${__dirname}/config`,
   '@controllers': `${__dirname}/controllers`,
+  '@database': `${__dirname}/database`,
   '@middlewares': `${__dirname}/middlewares`,
   '@routes': `${__dirname}/routes`,
 })
