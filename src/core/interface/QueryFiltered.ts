6512981f0ea5b927a8afa0f7b41dd28dcfc9a554
masb0ymas <n.fajri@outlook.com>
2023-04-02 15:43:23 +0700

    fix: interface query filtered

--- src/core/interface/QueryFiltered.ts
+++ src/core/interface/QueryFiltered.ts
@@ -1,6 +1,3 @@
-import { type Request } from 'express'
-import { type ObjectLiteral, type SelectQueryBuilder } from 'typeorm'
-
 export interface FilteredQueryEntity {
   id: string
   value: string
@@ -9,10 +6,4 @@ export interface FilteredQueryEntity {
 export interface SortedQueryEntity {
   sort: string
   order: 'ASC' | 'DESC'
-}
-
-export interface UseQueryEntity<T extends ObjectLiteral> {
-  entity: string
-  query: SelectQueryBuilder<T>
-  req: Request
 }
