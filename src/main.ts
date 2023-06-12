97c280943051f0fc01cc7c07ca34dd1c34d63984
masb0ymas <n.fajri@outlook.com>
2023-06-12 17:23:18 +0700

    fix: improve main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,16 @@
-import 'module-alias/register'
-import '~/core/modules/pathAlias'
-
 import http from 'http'
-import { App } from './config/app'
 import { env } from './config/env'
+import { App } from './config/app'
 import { httpHandle } from './core/modules/http/handle'
 
 function bootstrap(): void {
   const port = env.APP_PORT
 
-  // create express app
   const app = new App().create()
-
   const server = http.createServer(app)
 
-  // http handle
   const { onError, onListening } = httpHandle(server, port)
 
-  // run server listen
   server.listen(port)
   server.on('error', onError)
   server.on('listening', onListening)
