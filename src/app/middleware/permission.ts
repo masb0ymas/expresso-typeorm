9e4e48259654dd6babcd7d6d0009d6178ce0c89f
masb0ymas <n.fajri@outlook.com>
2023-09-07 14:02:17 +0700

    fix: improve permission middleware

--- src/app/middleware/permission.ts
+++ src/app/middleware/permission.ts
@@ -8,11 +8,48 @@ import { AppDataSource } from '~/database/data-source'
 import { User, type UserLoginAttributes } from '~/database/entities/User'
 
 /**
+ * Permission Access
+ * @param roles
+ * @returns
+ */
+export function permissionAccess(roles: string[]) {
+  return async (req: Request, res: Response, next: NextFunction) => {
+    const { lang } = req.getQuery()
+    const defaultLang = lang ?? env.APP_LANG
+    const i18nOpt: string | TOptions = { lng: defaultLang }
+
+    const userRepository = AppDataSource.getRepository(User)
+
+    const userLogin = req.getState('userLogin') as UserLoginAttributes
+    const getUser = await userRepository.findOne({
+      where: { id: userLogin.uid },
+    })
+
+    const errType = `permitted access error:`
+    const errMessage = 'you are not allowed'
+
+    if (getUser && !roles.includes(getUser.role_id)) {
+      const msgType = green('permission')
+      logger.error(`${msgType} - ${errType} ${errMessage}`)
+
+      const message = i18n.t('errors.permission_access', i18nOpt)
+
+      return res.status(403).json({
+        code: 403,
+        message: `${errType} ${message}`,
+      })
+    }
+
+    next()
+  }
+}
+
+/**
  * Not Permitted Access
  * @param roles
  * @returns
  */
-function notPermittedAccess(roles: string[]) {
+export function notPermittedAccess(roles: string[]) {
   return async (req: Request, res: Response, next: NextFunction) => {
     const { lang } = req.getQuery()
     const defaultLang = lang ?? env.APP_LANG
@@ -43,5 +80,3 @@ function notPermittedAccess(roles: string[]) {
     next()
   }
 }
-
-export default notPermittedAccess
