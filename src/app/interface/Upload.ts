734b20fc9001c8456b8372e8b927de7e82329a71
masb0ymas <n.fajri@outlook.com>
2023-10-03 16:37:09 +0700

    fix: update endpoint upload

--- src/app/interface/Upload.ts
+++ src/app/interface/Upload.ts
@@ -1,7 +1,13 @@
 import { type FileAttributes } from 'expresso-provider/lib/interface'
+import { Upload } from '~/database/entities/Upload'
 
 export interface UploadFileEntity {
   fieldUpload: FileAttributes
   directory: string
   upload_id?: string | undefined
+}
+
+export interface DtoUploadFile {
+  storage: any
+  upload: Upload
 }
