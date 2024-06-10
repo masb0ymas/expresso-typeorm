1849e25e60bd484bd11bee72fa05535cc32cd875
masb0ymas <n.fajri@outlook.com>
2024-06-10 14:58:33 +0700

    fix: prettier eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,9 @@
 module.exports = {
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:prettier/recommended'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
   plugins: ['prettier', '@typescript-eslint'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
