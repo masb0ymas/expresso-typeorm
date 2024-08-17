838fa4e58384c7a597adb4f07a0474de32e91b6e
masb0ymas <n.fajri@outlook.com>
2024-08-17 15:50:26 +0700

    refactor: implementation docs swagger

--- src/routes/api/v1.ts
+++ src/routes/api/v1.ts
@@ -1,9 +1,12 @@
-import express from 'express'
+import express, { Request, Response } from 'express'
+import swaggerUI from 'swagger-ui-express'
 import { AuthController } from '~/app/controller/auth.controller'
 import { RoleController } from '~/app/controller/role.controller'
 import { SessionController } from '~/app/controller/session.controller'
 import { UploadController } from '~/app/controller/upload.controller'
 import { UserController } from '~/app/controller/user.controller'
+import { env } from '~/config/env'
+import { optionsSwaggerUI, swaggerSpec } from '~/core/modules/getDocsSwagger'
 
 const route = express.Router()
 
@@ -12,5 +15,20 @@ route.use(SessionController)
 route.use(UploadController)
 route.use(UserController)
 route.use(AuthController)
+
+function docsSwagger() {
+  route.get('/api-docs.json', (_req: Request, res: Response) => {
+    res.setHeader('Content-Type', 'application/json')
+    res.send(swaggerSpec)
+  })
+
+  route.use('/api-docs', swaggerUI.serve)
+  route.get('/api-docs', swaggerUI.setup(swaggerSpec, optionsSwaggerUI))
+}
+
+// docs swagger disable for production mode
+if (env.NODE_ENV !== 'production') {
+  docsSwagger()
+}
 
 export { route as v1Routes }
