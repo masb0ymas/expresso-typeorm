c0e9512c60107595905da26f23c02de891d7d35b
masb0ymas <n.fajri@outlook.com>
2024-08-21 23:15:08 +0700

    feat: add utils test

--- src/core/utils/string.ts
+++ src/core/utils/string.ts
@@ -1,23 +1,15 @@
 /**
- *
- * @param string
- * @returns
+ * Capitalizes the first letter of each word in a string and removes special characters.
+ * @param str The input string to be capitalized.
+ * @returns The capitalized string with special characters removed.
  */
-export function capitalizeFirstLetter(string: string): string {
-  const regex = /[-`~!@#$%^&*_|=?;:'",<>]/gi
+export function capitalizeFirstLetter(str: string): string {
+  const specialCharsRegex = /[-`~!@#$%^&*_|=?;:'",<>]/gi;
 
-  const first_word = string.charAt(0).toUpperCase()
-  const last_word = string.slice(1)?.split(regex)?.join(' ')
-
-  const new_word = `${first_word}${last_word}`
-  const split_word = new_word.split(' ')
-
-  const result = split_word
-    .map((word) => {
-      const first_split_word = word.charAt(0).toUpperCase()
-      return `${first_split_word}${word.slice(1)}`
-    })
-    .join(' ')
-
-  return result
+  return str
+    .replace(specialCharsRegex, ' ')
+    .split(' ')
+    .filter(Boolean)
+    .map(word => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase())
+    .join(' ');
 }
